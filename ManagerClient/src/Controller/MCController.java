429d7383d0b8901bece5526a428db9e48d8cb171
Eric Volmer <eric.volmer@hotmail.com>
2020-03-03 11:41:39 +0100

    ANother update

--- ManagerClient/src/Controller/MCController.java
+++ ManagerClient/src/Controller/MCController.java
@@ -1,10 +1,18 @@
 package Controller;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import model.Employee;
 import serverinterface.IServer;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -31,4 +39,26 @@ public class MCController implements IMCController, Initializable {
 
     }
 
+    public void AddEmp(ActionEvent actionEvent)
+    {
+        Button btn = new Button();
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                Parent root;
+                try {
+                    root = FXMLLoader.load(getClass().getClassLoader().getResource("MCClient.fxml"));
+                    Stage stage = new Stage();
+                    stage.setTitle("Employee Added");
+                    stage.setScene(new Scene(root, 450, 450));
+                    stage.show();
+                    // Hide this current window (if this is what you want)
+                    ((Node)(event.getSource())).getScene().getWindow().hide();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 }
--- ManagerClient/src/View/MCClient.java
+++ ManagerClient/src/View/MCClient.java
@@ -1,9 +1,13 @@
 package View;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -21,5 +25,7 @@ public class MCClient extends Application {
         primaryStage.setScene(new Scene(main, 600, 400));
         primaryStage.show();
 
+
     }
+
 }
