256057deb61b2bdfb595a62b339bbfa926bc2512
Panth112802 <36990771+Panth112802@users.noreply.github.com>
2020-03-05 16:12:38 +0100

    ConnUpdate

--- Server/src/DB/EmpDAOTest.java
+++ Server/src/DB/EmpDAOTest.java
@@ -12,17 +12,17 @@ class EmpDAOTest {
     private EmpDAO empDAO;
     private Conn db;
     @BeforeEach
-    public void setUp() {
+    public void setUp() throws ClassNotFoundException {
 
 
-//            Conn db = new Conn();
+          Conn db = new Conn();
             empDAO = new EmpDAO();
 
     }
 
     @org.junit.jupiter.api.Test
     void addEmployee() {
-        Employee emp = new Employee("zsolt","E1987");
+        Employee emp = new Employee("zsolt","E1234");
          String test = empDAO.addEmployee(emp);
 
          assertSame("Employee added",test);
