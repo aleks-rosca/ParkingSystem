15d90d9385afdbfd1e5588bf315c9416a457bdfd
zsoltgorgenyi <45036238+zsoltgorgenyi@users.noreply.github.com>
2020-03-05 15:48:22 +0100

    Reservation

--- Server/src/DB/IEmpDAO.java
+++ Server/src/DB/IEmpDAO.java
@@ -1,11 +1,9 @@
 package DB;
 
 import model.Employee;
-import model.EmployeeRes;
 
 public interface IEmpDAO {
     String  addEmployee(Employee employee);
     boolean checkEmployeeByEmpNumber(String empnumber);
     Employee getEmployeeByName(String name);
-    void addReservetion(Employee employee, EmployeeRes employeeRes);
 }
