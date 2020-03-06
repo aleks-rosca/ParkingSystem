c72d9a741cf3edf16087792eebf9c3c2df22504e
Panth112802 <36990771+Panth112802@users.noreply.github.com>
2020-03-06 11:35:13 +0100

    tsfg

---
+++ Server/src/DB/EmpResDAOTest.java
@@ -0,0 +1,24 @@
+package DB;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmpResDAOTest {
+    IEmpResDAO test;
+    private Conn conn;
+    private EmpResDAO empResDAO;
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addReservetion() {
+    }
+}
