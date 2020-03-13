452be9fcbf8445ac8ba85f6120185450719dc8a7
Panth112802 <36990771+Panth112802@users.noreply.github.com>
2020-03-13 12:26:02 +0100

    updated empdao
    Co-Authored-By: zsoltgorgenyi <zsoltgorgenyi@users.noreply.github.com>

--- Server/src/DB/EmpDAO.java
+++ Server/src/DB/EmpDAO.java
@@ -33,7 +33,7 @@ public class EmpDAO implements IEmpDAO {
 
     public String addEmployee(Employee employee) {
 
-        String sql = "INSERT INTO employee values('" + employee.getEmpNumber() + "','" + employee.getEmpName() + "');";
+        String sql = "INSERT INTO employee values('" + employee.getEmpNumber() + "','" + employee.getEmpFirstName() + "','"+employee.getEmpLastName()+"');";
         try {
             conn.update(sql);
 
