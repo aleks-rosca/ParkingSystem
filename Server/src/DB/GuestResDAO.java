c734f03004260ef77e694e94cc6033bf97820115
Panth112802 <36990771+Panth112802@users.noreply.github.com>
2020-03-13 11:42:16 +0100

    test+dao
    Co-Authored-By: zsoltgorgenyi <zsoltgorgenyi@users.noreply.github.com>

--- Server/src/DB/GuestResDAO.java
+++ Server/src/DB/GuestResDAO.java
@@ -3,6 +3,8 @@ package DB;
 
 import model.GuestRes;
 
+import java.sql.SQLException;
+
 public class GuestResDAO implements IGuestResDAO{
     private Conn conn;
 
@@ -13,9 +15,25 @@ public class GuestResDAO implements IGuestResDAO{
 
     @Override
     public String addGuestRes(GuestRes guestRes) {
-        String sql = "INSERT INTO guestres values('"+guestRes.getDate()+"',"+guestRes.getResNo()+"',"
-                +guestRes.getName()+"',"+guestRes.getPurpose()"';";
+        String sql = "INSERT INTO guestres values('"+guestRes.getDate()+"','"+guestRes.getResNo()+"','"
+                +guestRes.getName()+"','"+guestRes.getPurpose()+"');";
+        try {
+            conn.update(sql);
 
-        return null;
+        } catch (SQLException e) {
+
+            if(e.getSQLState().equals("23505")){
+                return "Reservation for guest is not created";
+            }else if(e.getSQLState().equals("23514")){
+
+                return "You can not create reservation for same day or more than 7 days ahead";
+            }
+            e.printStackTrace();
+        }
+
+        return "Reservation for guest is created";
     }
+
+
+
 }
---
+++ Server/src/DB/GuestResDAOTest.java
@@ -0,0 +1,29 @@
+package DB;
+
+import model.GuestRes;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GuestResDAOTest {
+    IGuestResDAO guestResDAO;
+    GuestRes newGuest;
+    @BeforeEach
+    void setUp() {
+        guestResDAO = new GuestResDAO();
+        newGuest = new GuestRes("testGuest","testing","2020-03-14");
+        newGuest.setResNo("G1234");
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addGuestRes() {
+         String test =guestResDAO.addGuestRes(newGuest);
+        assertEquals("Reservation for guest is created",test);
+    }
+}
