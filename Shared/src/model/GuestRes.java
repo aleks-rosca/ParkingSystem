42366f95d968fad8bd22c338669e5eab548c7455
aleks-rosca <56295581+aleks-rosca@users.noreply.github.com>
2020-03-13 10:53:41 +0100

    Update GuestRes.java

--- Shared/src/model/GuestRes.java
+++ Shared/src/model/GuestRes.java
@@ -5,14 +5,12 @@ public class GuestRes {
     private String name;
     private String purpose;
     private String date;
-    private String resNo;
 
 
-    public GuestRes(String name, String purpose, String date, String resNo) {
+    public GuestRes(String name, String purpose, String date) {
         this.name = name;
         this.purpose = purpose;
         this.date = date;
-        this.resNo = resNo;
     }
 
     public String getName() {
@@ -38,12 +36,5 @@ public class GuestRes {
     public void setDate(String date) {
         this.date = date;
     }
-
-    public String getResNo() {
-        return resNo;
-    }
-
-    public void setResNo(String resNo) {
-        this.resNo = resNo;
-    }
+    
 }
