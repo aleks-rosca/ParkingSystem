2e5641af18cb6c7d98cd47c254dae6317641fbd4
Panth112802 <36990771+Panth112802@users.noreply.github.com>
2020-03-13 11:22:07 +0100

    Update GuestResDAO.java

--- Server/src/DB/GuestResDAO.java
+++ Server/src/DB/GuestResDAO.java
@@ -1,4 +1,21 @@
 package DB;
 
-public class GuestResDAO {
+
+import model.GuestRes;
+
+public class GuestResDAO implements IGuestResDAO{
+    private Conn conn;
+
+    public GuestResDAO(){
+        conn = Conn.getInstance();
+
+    }
+
+    @Override
+    public String addGuestRes(GuestRes guestRes) {
+        String sql = "INSERT INTO guestres values('"+guestRes.getDate()+"',"+guestRes.getResNo()+"',"
+                +guestRes.getName()+"',"+guestRes.getPurpose()"';";
+
+        return null;
+    }
 }
