bbca874a29a2d3d99796b4fa38848141f1e44fec
aleks-rosca <56295581+aleks-rosca@users.noreply.github.com>
2020-03-17 12:04:00 +0100

    Client

--- GateClient/src/Controller/GController.java
+++ GateClient/src/Controller/GController.java
@@ -34,21 +34,25 @@ public class GController {
             // create a alert
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setContentText("Please fill in the fields");
+            alert.show();
         }
         //model.empCheckIn(checkInTf.getText());
         else if(model.empCheckIn(checkInTf.getText()).equals("checked in to parking lot")){
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setContentText("Checked In");
+            alert.show();
 
         }
         else if(model.empCheckIn(checkInTf.getText()).equals("No such employee number")){
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setContentText("No such Employee");
+            alert.show();
 
         }
         else if(model.empCheckIn(checkInTf.getText()).equals("Already checked in")) {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setContentText("Already Checked In");
+            alert.show();
 
         }
 
