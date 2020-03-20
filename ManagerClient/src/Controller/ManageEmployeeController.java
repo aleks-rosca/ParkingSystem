f29a4e848fdf89c60c453b0cddfb9d54fbb3c327
Eric Volmer <eric.volmer@hotmail.com>
2020-03-20 13:32:40 +0100

    Employee Client gui update numeros 2000

--- ManagerClient/src/Controller/ManageEmployeeController.java
+++ ManagerClient/src/Controller/ManageEmployeeController.java
@@ -5,12 +5,14 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
 import model.Employee;
 
 import java.io.IOException;
 import java.util.Optional;
 
-public class ManageEmployeeController {
+public class ManageEmployeeController
+{
     public TextField empFirstNameAddTf;
     public TextField empLastNameAddTf;
     public TextField empNameSearchTf;
@@ -19,43 +21,51 @@ public class ManageEmployeeController {
     @FXML
     TableView<Employee> manageEmpTableTv;
     @FXML
-    TableColumn<Employee,String> manageEmpTable;
+    TableColumn<Employee, String> manageEmpTable;
     @FXML
-    TableColumn<Employee,String> manageEmpNo;
+    TableColumn<Employee, String> manageEmpNo;
     @FXML
-    TableColumn<Employee,String> empLast;
+    TableColumn<Employee, String> empLast;
 
-    public void init(IMCEModel mceModel) {
+    public void init(IMCEModel mceModel)
+    {
         this.mceModel = mceModel;
     }
 
 
-    public void AddEmp(ActionEvent actionEvent) throws IOException {
+    public void AddEmp(ActionEvent actionEvent) throws IOException
+    {
         //TODO add a try catch to see if the name and number exist already\
-        if(!(empFirstNameAddTf.getText().isEmpty())&!(empLastNameAddTf.getText().isEmpty())&!(empNumberAddTf.getText().isEmpty())){
-            if (mceModel.checkFirstChar(empNumberAddTf.getText())) {
+        if (!(empFirstNameAddTf.getText().isEmpty()) & !(empLastNameAddTf.getText().isEmpty()) & !(empNumberAddTf.getText().isEmpty()))
+        {
+            if (mceModel.checkFirstChar(empNumberAddTf.getText()))
+            {
                 String temp = mceModel.createEmp(mceModel.rearrangeChars(empFirstNameAddTf.getText()), empLastNameAddTf.getText(), empNumberAddTf.getText());
-                if (temp.equals("Employee added")) {
+                if (temp.equals("Employee added"))
+                {
                     Alert a1 = new Alert(Alert.AlertType.INFORMATION, "Employee " + empFirstNameAddTf.getText() + " has been added!", ButtonType.OK);
                     a1.show();
                     empFirstNameAddTf.clear();
                     empNumberAddTf.clear();
-                } else if (temp.equals("Duplicate key")) {
+                } else if (temp.equals("Duplicate key"))
+                {
                     Alert a1 = new Alert(Alert.AlertType.INFORMATION, "Employee " + empFirstNameAddTf.getText() + " already exists!", ButtonType.OK);
                     a1.show();
-                } else {
+                } else
+                {
                     Alert a1 = new Alert(Alert.AlertType.INFORMATION, "Error 404", ButtonType.OK);
                     a1.show();
                 }
-            } else {
+            } else
+            {
                 Alert a1 = new Alert(Alert.AlertType.INFORMATION, "Employee number should start from E", ButtonType.OK);
                 a1.show();
             }
-        }else{
-            Alert a1 = new Alert(Alert.AlertType.INFORMATION, "All of the fields should be filled" , ButtonType.OK);
+        } else
+        {
+            Alert a1 = new Alert(Alert.AlertType.INFORMATION, "All of the fields should be filled", ButtonType.OK);
             a1.show();
         }
-
 
 
 //        if (model.createEmp()) {
@@ -74,32 +84,37 @@ public class ManageEmployeeController {
 //        }
     }
 
-    public void empSearchBtn(ActionEvent actionEvent) {
+    public void empSearchBtn(ActionEvent actionEvent)
+    {
          /*   try
         {
             //TODO display empNameSearchTf.getText() in the ListView
         }
         catch (Exception e)
         { */
-        if (empNameSearchTf.getText().trim().isEmpty()) {
+        if (empNameSearchTf.getText().trim().isEmpty())
+        {
             // create a alert
             Alert a1 = new Alert(Alert.AlertType.ERROR, "Employee Search field is empty ", ButtonType.OK);
             a1.show();
 
-        } else {
+        } else
+        {
             // create a alert
             Alert a1 = new Alert(Alert.AlertType.ERROR, "Employee " + empNameSearchTf.getText() + " does not exist in the system" + " \n\nAdd Employee to the system? ", ButtonType.YES, ButtonType.NO);
 
             Optional<ButtonType> result = a1.showAndWait();
             ButtonType button = result.orElse(ButtonType.NO);
-            if (button == ButtonType.YES) {
+            if (button == ButtonType.YES)
+            {
 
                 String change = empNameSearchTf.getText();
                 empFirstNameAddTf.setText(change);
                 empNameSearchTf.clear();
 
 
-            } else {
+            } else
+            {
                 System.out.println("No pressed");
             }
 
@@ -107,29 +122,88 @@ public class ManageEmployeeController {
 
     }
 
-    public void showAllEmployee(ActionEvent actionEvent) {
+    public void showAllEmployee(ActionEvent actionEvent)
+    {
         manageEmpTable.setCellValueFactory(new PropertyValueFactory<>("empFirstName"));
-        empLast.setCellValueFactory(new PropertyValueFactory< >("empLastName"));
-        manageEmpNo.setCellValueFactory(new PropertyValueFactory< >("empNumber"));
+        empLast.setCellValueFactory(new PropertyValueFactory<>("empLastName"));
+        manageEmpNo.setCellValueFactory(new PropertyValueFactory<>("empNumber"));
 
         manageEmpTableTv.setItems(mceModel.getAllEmployee());
 
 
     }
+
     public void editEmp(ActionEvent actionEvent)
     {
-        dialog.setTitle("Text Input Dialog");
-        dialog.setHeaderText("Look, a Text Input Dialog");
-        dialog.setContentText("Please enter your name:");
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
+        alert.setTitle("Editing Employee");
+        alert.setHeaderText("Chose the edits you want to do");
+
+        Label label1 = new Label("Employee First Name: ");
+        Label label2 = new Label("Employee Last Name: ");
+        Label label3 = new Label("Employee Number: ");
+        TextField text1 = new TextField();
+        TextField text2 = new TextField();
+        TextField text3 = new TextField();
+
+        GridPane grid = new GridPane();
+        grid.add(label1, 1, 1);
+        grid.add(text1, 2, 1);
+        grid.add(label2, 1, 2);
+        grid.add(text2, 2, 2);
+        grid.add(label3, 1, 3);
+        grid.add(text3, 2, 3);
+        alert.getDialogPane().setContent(grid);
+
 
 // Traditional way to get the response value.
-        Optional<String> result = dialog.showAndWait();
-        if (result.isPresent())
-        {
-            System.out.println("Your name: " + result.get());
-        }
 
-// The Java 8 way to get the response value (with lambda expression).
-        result.ifPresent(name -> System.out.println("Your name: " + name));
+        alert.getButtonTypes().clear();
+        ButtonType updateButton = new ButtonType("Confirm");
+        ButtonType deleteButton = new ButtonType("Delete");
+        alert.getButtonTypes().setAll(updateButton, deleteButton);
+
+
+        alert.getButtonTypes().clear();
+        alert.getButtonTypes().setAll(updateButton, deleteButton);
+        alert.showAndWait().ifPresent(type ->
+        {
+            if (type == updateButton)
+            {
+                if (!(text1.getText().isEmpty()) & !(text2.getText().isEmpty()) & !(text3.getText().isEmpty()))
+                {
+
+                } else
+                {
+                    Alert a1 = new Alert(Alert.AlertType.INFORMATION, "All of the fields should be filled", ButtonType.OK);
+                    a1.showAndWait();
+                    alert.show();
+                }
+
+                System.out.println("Updated");
+            }
+            else if (type == deleteButton)
+            {
+                System.out.println("Deleting....");
+                Alert alert2 = new Alert(Alert.AlertType.CONFIRMATION);
+                alert2.setTitle("Current project is modified");
+                alert2.setContentText("Save?");
+                ButtonType okButton = new ButtonType("Yes", ButtonBar.ButtonData.YES);
+                ButtonType cancelButton = new ButtonType("No", ButtonBar.ButtonData.CANCEL_CLOSE);
+                alert2.getButtonTypes().setAll(okButton, cancelButton);
+                alert2.showAndWait().ifPresent(type2 ->
+                {
+                    if (type2 == okButton)
+                    {
+                        System.out.println("Deleted");
+                    } else
+                    {
+                        System.out.println("Not Deleted");
+                    }
+                });
+            }
+        });
     }
 }
