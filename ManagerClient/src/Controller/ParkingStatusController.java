cd3c9df86e542a8ceb48ff73baf05d00526659f1
aleks-rosca <56295581+aleks-rosca@users.noreply.github.com>
2020-03-25 17:17:11 +0100

    pie

--- ManagerClient/src/Controller/ParkingStatusController.java
+++ ManagerClient/src/Controller/ParkingStatusController.java
@@ -2,12 +2,17 @@ package Controller;
 
 import Model.IMCPModel;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import model.Status;
 
 import java.net.URL;
@@ -15,24 +20,31 @@ import java.util.ResourceBundle;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class ParkingStatusController implements Initializable
-{
-    private IMCPModel model;
+public class ParkingStatusController implements Initializable {
+    public PieChart pieChart;
+    public Label chartLabel;
     public Label totalSpotsLabel;
     public Button statusBtn;
-    public TableColumn<Status,String> statusDate;
-    public TableColumn<Status,Integer> statusSpots;
+    public TableColumn<Status, String> statusDate;
+    public TableColumn<Status, Integer> statusSpots;
     public TableView<Status> statusTable;
+    public int e;
+    public int g;
+    public int p;
+    private IMCPModel model;
 
-
-    public void init( IMCPModel model) {
+    public void init(IMCPModel model) {
         this.model = model;
         getCurrentStatus();
         showAllStatues();
+        updatePie();
+        e = model.getNumberOfEmployeesInParkingLot();
+        g = model.getNumberOfGuestsInParkingLot();
+        p = model.getNumberOfPublicUsersInParkingLot();
 
     }
 
-    public void showAllStatues(){
+    public void showAllStatues() {
         statusDate.setCellValueFactory(new PropertyValueFactory<>("date"));
         statusSpots.setCellValueFactory(new PropertyValueFactory<>("occupiedSpots"));
 
@@ -40,20 +52,48 @@ public class ParkingStatusController implements Initializable
 
 
     }
+
+    public void updatePie() {
+        System.out.println(e + " e");
+        System.out.println(g + " g");
+        System.out.println(p + " p");
+        ObservableList<PieChart.Data> pieChartData =
+                FXCollections.observableArrayList(
+                        new PieChart.Data("Employees", e),
+                        new PieChart.Data("Guests", g),
+                        new PieChart.Data("Public Users", p));
+
+
+        pieChart.setData(pieChartData);
+
+        for (PieChart.Data data : pieChart.getData()) {
+            data.getNode().addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+
+                    updatePie();
+                }
+            });
+        }
+    }
+
     public void getCurrentStatus() {
         int MINUTES = 1;
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                Platform.runLater(()->totalSpotsLabel.setText(String.valueOf(40-model.getOccupiedSpots())));
+                Platform.runLater(() -> totalSpotsLabel.setText(String.valueOf(40 - model.getOccupiedSpots())));
             }
         }, 0, 1000 * 6 * MINUTES);
 
     }
 
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+
     }
 }
+
